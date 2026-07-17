0dfac918151dab6d34f92f6b4b921b21244dc0b0
Mitchell Sotto <ms084743@flooranddecor.com>
2026-07-17 17:41:00 -0400

    Auto-commit: 2026-07-17 17:41:00

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -9,6 +9,12 @@ GIT_EXEC="/usr/bin/git"
 # Print a timestamp separator
 echo "=== Run at $(date '+%Y-%m-%d %H:%M:%S') ==="
 
+# Limit the log file to the last 1000 lines
+LOG_FILE="/Users/Mitchell.Sotto/BrainGarden/git_push_log.log"
+if [ -f "$LOG_FILE" ]; then
+    tail -n 1000 "$LOG_FILE" > "${LOG_FILE}.tmp" && mv "${LOG_FILE}.tmp" "$LOG_FILE"
+fi
+
 # Execute Git commands (standard output and errors will go to the log)
 $GIT_EXEC add .
 $GIT_EXEC commit -m "Auto-commit: $(date '+%Y-%m-%d %H:%M:%S')"
